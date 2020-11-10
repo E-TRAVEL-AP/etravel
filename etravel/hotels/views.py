4f5cc1a6e26157248f8160edfd4ff7b735381e1d
Rishabh Goswami <rishabh.goswami_ug22@ashoka.edu.in>
2020-11-11 00:46:17 +0530

    updated the base file

--- etravel/hotels/views.py
+++ etravel/hotels/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
-import requests
 from . import fakemovies
 
 
@@ -13,4 +12,4 @@ hotels =   fakemovies.hotels
 
 def index(request):
     title = "Etravel"
-    return render(request, "index.html", {'hotels': hotels, 'title': title})
+    return render(request, "hotels/index.html", {'hotels': hotels, 'title': title})
