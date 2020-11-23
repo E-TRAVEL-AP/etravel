e8c4a5cdb948da2f298651b258ea3b2271b94886
Rishabh Goswami <rishabh.goswami_ug22@ashoka.edu.in>
2020-11-23 16:11:54 +0530

    made changes to template folder

--- etravel/hotels/urls.py
+++ etravel/hotels/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.about, name="about"),
+    path('', views.homepage, name="homepage"),
     path('register/', views.register, name="register"), 
     path('login/', views.login_request, name="login"), 
     path('logout/', views.logout_request, name="logout"),
